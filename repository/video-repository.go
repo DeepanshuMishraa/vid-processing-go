68f4d4d3648203139ad0cd1ed6ac201323e533d0
Deepanshu Mishra <d4deepanshu723@gmail.com>
2026-06-28 10:20:35 +0530

    add video repo functions

--- repository/video-repository.go
+++ repository/video-repository.go
@@ -30,3 +30,61 @@ func CreateVideo(db *pgxpool.Pool, video models.Video) error {
 
 	return nil
 }
+
+func GetAllVideos(db *pgxpool.Pool) ([]models.Video, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	query := `SELECT id, title, original_url, status, created_at, updated_at
+	           FROM videos`
+
+	rows, err := db.Query(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var videos []models.Video
+	for rows.Next() {
+		var video models.Video
+		err := rows.Scan(
+			&video.ID,
+			&video.Title,
+			&video.OriginalURL,
+			&video.Status,
+			&video.CreatedAt,
+			&video.UpdatedAt,
+		)
+		if err != nil {
+			return nil, err
+		}
+		videos = append(videos, video)
+	}
+
+	return videos, nil
+}
+
+func GetVideoById(db *pgxpool.Pool, id string) (*models.Video, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	query := `SELECT id, title, original_url, status, created_at, updated_at
+	           FROM videos
+	           WHERE id = $1`
+	var video models.Video
+	err := db.QueryRow(ctx, query, id).Scan(
+		&video.ID,
+		&video.Title,
+		&video.OriginalURL,
+		&video.Status,
+		&video.CreatedAt,
+		&video.UpdatedAt,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &video, nil
+
+}
